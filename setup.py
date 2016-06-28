79ff27cd72d2eb31f6e37de89e3548f57718f7df
Chandan <cs3hq@virginia.edu>
2016-06-28 11:20:05 -0400

    setup for uploading to pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='zwatershed',
+      version='0.1',
+      description='Fast watersheds',
+      url='https://github.com/TuragaLab/zwatershed',
+      author='Chandan Singh',
+      author_email='csinva@virginia.edu',
+      license='MIT',
+      packages=['zwatershed'],
+      zip_safe=False)
