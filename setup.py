058d50e2550ee1bfd583fbf708b1371f9c0990f2
Chandan <cs3hq@virginia.edu>
2016-06-29 13:05:11 -0400

    added setup_requires

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 import os
 setup(name='zwatershed',
-      version='0.2',
+      version='0.3',
       description='Fast watersheds',
       url='https://github.com/TuragaLab/zwatershed',
       author='Chandan Singh',
       author_email='csinva@virginia.edu',
       license='MIT',
       install_requires=['cython','numpy','h5py'],	
+	  setup_requires=['cython','numpy'],	
       packages=['zwatershed'],
       zip_safe=False)
 os.system('./make.sh')
